6805f502c56b066186488edbc157828199bc9488
iampato <858wpwaweru@gmail.com>
2021-10-31 15:08:16 +0300

    added axois and albums respository

---
+++ SaltMusic/src/repository/albums_repository.ts
@@ -0,0 +1,28 @@
+import { Convert, TopAlbumsModel } from "../models/top_albums_model";
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://itunes.apple.com/us/rss",
+    timeout: 30000,
+});
+
+export namespace AlbumsRespository {
+
+    export async function getTopAlbums(): Promise<[TopAlbumsModel | null, string | null]> {
+        const endPoint = "/topalbums/limit=100/json";
+        try {
+            let response = await instance.get(endPoint);
+
+            if (response?.status === 200) {
+                let albums = Convert.toTopAlbumsModel(response.data);
+                return [albums, null];
+            } else {
+                return [null, "An error occurred"];
+            }
+        }
+        catch (e) {
+            return [null, (e as Error).message]
+        }
+    }
+
+}
