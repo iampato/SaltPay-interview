25e32a1ec7a990c04c3ccc7792f7953840afcb5c
iampato <858wpwaweru@gmail.com>
2021-10-31 16:50:13 +0300

    integrated with provider

--- SaltMusic/App.tsx
+++ SaltMusic/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import AppNavigation from "./src/navigation/stack";
+import { Provider } from "react-redux";
+import store from "./src/redux/album_store";
 
 const App = () => {
   return (
-    <AppNavigation />
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
   );
 };
 
